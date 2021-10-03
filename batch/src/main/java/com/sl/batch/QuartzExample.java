b70e52d5af5361d67bf2618c511fea1e32935a4a
Silvio Limeira <sicemal@gmail.com>
2021-10-03 12:39:07 -0300

    change to intelij

--- batch/src/main/java/com/sl/batch/QuartzExample.java
+++ batch/src/main/java/com/sl/batch/QuartzExample.java
@@ -19,7 +19,7 @@ import java.util.HashMap;
 public class QuartzExample {
         
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         SchedulerFactory schedFact = new StdSchedulerFactory();
         try {
---
+++ web/src/main/java/br/com/sl/Project1Application.java
@@ -0,0 +1,22 @@
+package br.com.sl;
+
+
+import com.sl.batch.QuartzExample;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1Application.class, args);
+	}
+
+	@PostConstruct
+	public void batch() {
+		QuartzExample.test();
+	}
+
+}

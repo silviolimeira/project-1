e96a46d642c8845635cc7505ff3a54fec1b2748e
Silvio Limeira <sicemal@gmail.com>
2021-10-03 10:46:21 -0300

    Test batch quartz int /home end point

--- web/src/main/java/br/com/sl/controller/HomeController.java
+++ web/src/main/java/br/com/sl/controller/HomeController.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.sl.tool.Tool;
+//import com.sl.tool.Tool;
+import com.sl.batch.*;
 
 @Controller
 public class HomeController {
@@ -13,8 +14,12 @@ public class HomeController {
 	public String sayHello(Model theModel) {
 		theModel.addAttribute("dateServer", new java.util.Date());
 
-		Tool tool = new Tool();
-		System.out.println(tool.getGreeting());
+		//Tool tool = new Tool();
+		//System.out.println(tool.getGreeting());
+		
+		// Test Quartz
+		//QuartzExample.test();
+
 
 		return "home";
 	}

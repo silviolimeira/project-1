9dc3f80649843f363e793e529ff2e77232411d46
Silvio Limeira <sicemal@gmail.com>
2021-11-06 16:09:23 -0300

    Implement YAML configuration, example implement PropertySourceFactory to access application.properties format

--- jwt/src/main/java/com/sl/jwt/service/InfoController.java
+++ jwt/src/main/java/com/sl/jwt/service/InfoController.java
@@ -1,18 +1,32 @@
 package com.sl.jwt.service;
 
+import com.sl.jwt.config.YamlPropertySourceFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ConfigurationProperties(prefix = "yaml")
+@PropertySource(value = "classpath:application.yml", factory= YamlPropertySourceFactory.class)
 public class InfoController {
 
     @Value("${server.port}")
     private Integer serverPort;
 
-    @GetMapping("/info")
-    public void infoGetMethod() {
+    @Value("${spring.profiles.active}")
+    private String springProfiles;
+
+
+    @GetMapping(value = "/info", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String infoGetMethod() {
+
         System.out.println(String.format("\n>>> Server port: %d\n", serverPort));
+        System.out.println("sprintProfies " + springProfiles);
+
+        return String.format("{ \"sprintProfies\": \"%s\" }", springProfiles);
     }
 
 }

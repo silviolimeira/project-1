912eb289be7a0387daf5429d13c465c7a7d0cc87
Silvio Limeira <sicemal@gmail.com>
2022-04-21 09:52:06 -0300

    criacao da estrutura do projeto

--- jsp-hibernate-2/src/main/resources/create_db.sql
+++ jsp-hibernate-2/src/main/resources/create_db.sql
@@ -6,3 +6,25 @@ CREATE TABLE employee (
     address VARCHAR(255),
     phoneNo VARCHAR(255)
 );
+
+-- NOVO PROJETO JSP
+CREATE TABLE departamento (
+    IdeDep INTEGER NOT NULL AUTO_INCREMENT,
+    NomDep VARCHAR(50) NULL,
+    TelDep VARCHAR(15) NULL,
+    PRIMARY KEY (IdeDep)
+);
+
+CREATE TABLE funcionario (
+    IdeFun INTEGER NOT NULL AUTO_INCREMENT,
+    IdeDep INTEGER NOT NULL,
+    NomFun VARCHAR(50) NULL,
+    TelFun VARCHAR(15) NULL,
+    CarFun VARCHAR(30) NULL,
+    SalFun DECIMAL(15,2) NULL,
+    PRIMARY KEY(IdeFun),
+    FOREIGN KEY(IdeDep)
+        REFERENCES departamento(IdeDep)
+            ON DELETE CASCADE
+            ON UPDATE NO ACTION
+);
